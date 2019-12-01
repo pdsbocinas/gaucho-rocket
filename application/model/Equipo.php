8b970a765997752e3c11df5516316bd528ced1cd
Pablo <pds.gomez@gmail.com>
2019-12-01 11:34:14 -0300

    fix en consulta de obtenerVuelosPorEquipoyCapacidad

--- application/model/Equipo.php
+++ application/model/Equipo.php
@@ -27,7 +27,7 @@
       $sql = "SELECT vuelo_id,count(vuelo_id) cantidad,vuelo.avion_id,equipo.id equipo,equipo.general,equipo.familiar,equipo.suite,(equipo.general+equipo.familiar+equipo.suite) as totalEquipo
               FROM asiento JOIN vuelo ON asiento.vuelo_id=vuelo.id
               JOIN equipo ON  vuelo.avion_id=equipo.avion_id
-              GROUP BY vuelo_id;";
+              GROUP BY vuelo_id, equipo.id;";
         $query = $this->database->query($sql);
         $result = $query->fetch_all(MYSQLI_ASSOC);
         return json_encode($result, true);
