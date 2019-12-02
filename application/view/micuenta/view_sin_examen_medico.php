0054ef44f74623ae7c00ab159246a2386485ee1e
Pablo <pds.gomez@gmail.com>
2019-12-01 23:47:55 -0300

    mejoras en la busqueda de vuelos, paso logica de php a js y fixeo boton de centros

--- application/view/micuenta/view_sin_examen_medico.php
+++ application/view/micuenta/view_sin_examen_medico.php
@@ -1,33 +1,12 @@
 <div class="container">
-  <div class="row">
+  <div class="row mt-4">
   <?php
     foreach ($data as $fila) {
-      echo "<div class='col-md-4'>";
+      echo "<div class='card col-md-4 mr-3' style='width: 18rem;'>";
       include($path->getPage("view", "components/card_centros.php"));
-      echo "<a id=centro-". $fila['id'] ." class='centro-medico'>Pedir turno</a>";
+      echo "<a id=centro-". $fila['id'] ." href='javascript:void(0)' class='btn btn-primary centro-medico'>Pedir turno</a>";
       echo "</div>";
     }
   ?>
   </div>
 </div>
-<script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
-<script type="text/javascript">
-  $(document).ready(function(){
-    $('.centro-medico').on('click', function(e) {
-      const regexOnlyNumber = new RegExp('([0-9.,]+)', 'g');
-      const getId = this.id.match(regexOnlyNumber).shift();
-      $.ajax({
-        type: "POST",
-        url: "<?php echo $path->getEvent('micuenta', 'crearTurno') ?>",
-        data: { centro_id: getId },
-        success: function(response) {
-          var link =  "<?php echo $this->path->getEvent('micuenta', 'examenes') ?>";
-          window.location.href = link
-        }
-      })
-    })
-  });
-</script>
