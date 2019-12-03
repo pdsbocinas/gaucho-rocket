96e07c4339803b72fa82144e2781b3bd7bf32042
Pablo <pds.gomez@gmail.com>
2019-12-03 14:38:32 -0300

    agrego fixes de enteros

--- application/model/Pdf.php
+++ application/model/Pdf.php
@@ -5,20 +5,14 @@ class PDF extends FPDF {
 
   function Header() {
     $this->SetFont('Arial','B',15);
-    // Movernos a la derecha
     $this->Cell(80);
-    // Título
     $this->Cell(50,10,'Gaucho Rocket',1,0,'C');
-    // Salto de línea
     $this->Ln(20);
   }
 
   function Footer() {
-    // Posición: a 1,5 cm del final
     $this->SetY(-15);
-    // Arial italic 8
     $this->SetFont('Arial','I',8);
-    // Número de página
     $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
   }
 }
