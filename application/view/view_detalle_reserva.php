b5026b23372ba173e248df6c2a9babfeffbe3558
Pablo <pds.gomez@gmail.com>
2019-12-03 13:50:23 -0300

    validacion de session cuando quiera reserva un vuelo agotado

--- application/view/view_detalle_reserva.php
+++ application/view/view_detalle_reserva.php
@@ -4,12 +4,16 @@
       include($path->getPage("view", "components/form_reservas.php"));
       echo "<p class='mt-2'>Ingresa o registrate para podes reservar</p>";
     } else if(!$data['disponibilidad']) {
-      echo 
-      "<form method='POST' class='confirm_lista_espera'>
-        <input type='hidden' name='usuario' value=".$_SESSION['userId']." />
-        <input type='hidden' name='vuelo' value=".$data[0]['id'].">
-        <button type='submit' class='btn btn-primary'>Anotarme en lista de espera</button>
-      </form>";
+      echo  "<form method='POST' class='confirm_lista_espera'>";
+      echo "<input type='hidden' name='usuario' value=".$_SESSION['userId']." />";
+      echo "<input type='hidden' name='vuelo' value=".$data[0]['id'].">";
+      if (is_null($_SESSION['userId'])) {
+        echo "<button type='reset' data-toggle='modal' data-target='#exampleModal' class='btn btn-primary white trigger-modal'>Anotarme en lista de espera</button>";
+      } else {
+        echo "<button type='submit' class='btn btn-primary'>Anotarme en lista de espera</button>";
+      }
+      echo "</form>";
+      
       include($path->getPage("view", "components/toast_success.php"));
     } else {
       include($path->getPage("view", "components/form_reservas.php"));
